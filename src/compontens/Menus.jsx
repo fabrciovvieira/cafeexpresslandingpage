bafb2d020b468090120f274f8329083b7ff359c8
fabrciovvieira <fabriciovvieira@gmail.com>
2024-10-29 13:01:47 -0300

    ajustei imagem bg 2

--- src/compontens/Menus.jsx
+++ src/compontens/Menus.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logoCafe.png'
 
 const Menus = () => {
   return (
-    <div className="w-full h-screen bg-menuBg relative bg-cover  bg-no-repeat">
+    <div className="w-full h-screen bg-menuBg relative bg-cover bg-no-repeat">
         <div className="flex flex-col justify-center items-center absolute top-[450px] phone:top-[600px] phone2:top-[300px] w-full m-auto">
             <img src={logo} alt="logo" />
             <p className="text-4xl font-raleway text-fontColours phone:text-2xl phone:absolute phone:top-[160px] phone:font-thin phone2:top-[200px] phone2:hidden">Cafeteria completa para eventos</p>
