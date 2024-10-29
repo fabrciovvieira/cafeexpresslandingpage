6aa8d0b96654289f264daf9ac20f43662001740a
fabrciovvieira <fabriciovvieira@gmail.com>
2024-10-29 12:33:44 -0300

    criei loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,28 @@ import { Footer } from "./compontens/Footer"
 import Hero from "./compontens/Hero"
 import Menus from "./compontens/Menus"
 import Services from "./compontens/Services"
+import { useState, useEffect } from "react"
+import logo from './assets/logoCafe.png'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulando o carregamento do background
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 2 segundos para simular o carregamento
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen justify-center items-center bg-bgHero bg-cover bg-no-repeat">
+        <div className="text-3xl font-raleway bg-background w-screen h-screen m-auto flex justify-center items-center"><img src={logo} alt="logo" /></div>
+      </div>
+    );
+  }
 
   return (
     <div className="h-screen bg-bgHero bg-cover bg-no-repeat">
